bcb9a4f9107bf6aad47730aceecefcc3c40cf9fc
Rafael Montenegro M. da Silva <88899571+rafamms013@users.noreply.github.com>
2024-01-27 22:26:02 -0300

    Add files via upload
    Um monte de pequenas alteracoes

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+const botao = document.querySelector(".btn-plataforma");
+const elementoPlataformas = document.querySelector(".btn-plataforma . plataformas");
+botao.addEventListener("click", () => {
+    elementoPlataformas.classList.add("ativo");
+
+    const botaoEstaAberto = elementoPlataformas.classList.
+    contains("ativo");
+
+    if(botaoEstaAberto){
+        elementoPlataformas.classList.remove("ativo");
+    }else{
+        elementoPlataformas.classList.add("ativo");
+    }
+});
